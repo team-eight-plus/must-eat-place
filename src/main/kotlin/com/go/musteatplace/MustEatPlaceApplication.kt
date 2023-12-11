c9542fea1a080c632493e9329dfde809672d7a4a
elena <creative.apme@gmail.com>
2023-12-12 01:57:36 +0900

    first commit

---
+++ src/main/kotlin/com/go/musteatplace/MustEatPlaceApplication.kt
@@ -0,0 +1,11 @@
+package com.go.musteatplace
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MustEatPlaceApplication
+
+fun main(args: Array<String>) {
+    runApplication<MustEatPlaceApplication>(*args)
+}
---
+++ src/test/kotlin/com/go/musteatplace/MustEatPlaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.go.musteatplace
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MustEatPlaceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
