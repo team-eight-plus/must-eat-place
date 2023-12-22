9bf552ff37649a0013cb07aff2c7864b217de419
elena <creative.apme@gmail.com>
2023-12-22 22:17:12 +0900

    fix: SearchService 예외 처리 추가

--- src/main/kotlin/com/go/musteatplace/search/application/SearchService.kt
+++ src/main/kotlin/com/go/musteatplace/search/application/SearchService.kt
@@ -1,13 +1,16 @@
 package com.go.musteatplace.search.application
 
+import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.go.musteatplace.search.presentation.dto.NaverSearchResponse
 import com.go.musteatplace.search.presentation.dto.SearchRequest
 import com.go.musteatplace.search.presentation.dto.SearchResponse
 import com.go.musteatplace.search.presentation.dto.SearchResultsDto
+import org.hibernate.service.spi.ServiceException
 import org.springframework.http.RequestEntity
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 import java.net.URLEncoder
@@ -17,10 +20,10 @@ import java.nio.charset.StandardCharsets
 @Service
 class SearchService(private val objectMapper: ObjectMapper) {
   fun getSearchResults(searchParam: SearchRequest): SearchResponse {
-        val (keyword) = searchParam
         val naverOpenApiId = System.getenv("NAVER_CLIENT_ID")
         val naverOpenApiSecret = System.getenv("NAVER_CLIENT_SECRET")
 
+        val (keyword) = searchParam
         val encodedKeyword = URLEncoder.encode(keyword, StandardCharsets.UTF_8.toString())
 
         val uri = UriComponentsBuilder
@@ -42,16 +45,24 @@ class SearchService(private val objectMapper: ObjectMapper) {
             .header("X-Naver-Client-Secret", naverOpenApiSecret)
             .build()
 
-        val res = restTemplate.exchange(req, String::class.java)
-        val searchResults = parseSearchResults(res.body)
+        try {
+          val res = restTemplate.exchange(req, String::class.java)
+          val searchResults = parseSearchResults(res.body)
 
-        return SearchResponse(keyword, searchResults)
+          return SearchResponse(keyword, searchResults)
+        } catch (e: RestClientException) {
+          throw ServiceException("Failed to fetch search results", e)
+        }
     }
 
     fun parseSearchResults(json: String?): List<SearchResultsDto> {
       json ?: return emptyList()
 
-      val naverSearchResponse = objectMapper.readValue<NaverSearchResponse>(json)
-      return naverSearchResponse.items
+      try {
+        val naverSearchResponse = objectMapper.readValue<NaverSearchResponse>(json)
+        return naverSearchResponse.items
+      } catch (e: JsonProcessingException) {
+        throw ServiceException("Error parsing search results", e)
+      }
   }
 }
