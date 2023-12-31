c7f81441d3f42735165824a2ede3cb25b52ec5f8
elena <creative.apme@gmail.com>
2023-12-31 17:53:16 +0900

    fix: 불필요한 파일 및 라이브러리 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-jdbc")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
-  implementation("org.springframework.boot:spring-boot-starter-webflux")
 
   implementation("mysql:mysql-connector-java:8.0.31")
 
