a60401f6b117c2a389e13c06f0506af78786217d
elena <creative.apme@gmail.com>
2024-01-10 02:11:34 +0900

    fix: save 로직 수정

--- src/main/kotlin/com/go/musteatplace/search/application/SearchService.kt
+++ src/main/kotlin/com/go/musteatplace/search/application/SearchService.kt
@@ -1,14 +1,38 @@
 package com.go.musteatplace.search.application
 
+import com.go.musteatplace.search.domain.SearchHistory
 import com.go.musteatplace.search.presentation.dto.*
 import org.springframework.stereotype.Service
 
 @Service
 class SearchService(
-  private val searchManager: SearchWorker
+  private val searchManager: SearchWorker,
+  private val searchAppender: SearchAppender
 ) {
   fun search(request: SearchRequest): List<SearchResultsDto>? {
-    val res = searchManager.searchActual(request) ?: throw RuntimeException("Fail to get search result")
-    return res?.let { searchManager.parseSearchResults(it) }
+    val searchResultsJson = searchManager.searchActual(request)
+
+    if (searchResultsJson != null) {
+      val searchResults = searchManager.parseSearchResults(searchResultsJson)
+
+      if (searchResults?.isNotEmpty() == true) {
+        val firstResult = searchResults[0]
+        val searchHistory = SearchHistory(
+          keyword = request.keyword,
+          name = firstResult.name,
+          url = firstResult.url,
+          address = firstResult.address,
+          road_address = firstResult.roadAddress,
+          phone = firstResult.phone,
+          category = firstResult.category,
+          mapx = firstResult.mapx,
+          mapy = firstResult.mapy
+        )
+        searchAppender.save(searchHistory)
+      }
+      return searchResults
+    } else {
+      throw RuntimeException("Fail to get search result")
+    }
   }
 }
--- src/main/kotlin/com/go/musteatplace/search/domain/SearchHistory.kt
+++ src/main/kotlin/com/go/musteatplace/search/domain/SearchHistory.kt
@@ -12,7 +12,7 @@ class SearchHistory(
   val url: String,
   val category: String,
   val address: String,
-  val roadAddress: String,
+  val road_address: String,
   val phone: String,
   val mapx: String,
   val mapy: String
@@ -30,7 +30,7 @@ class SearchHistory(
         url = target.searchResults[0].url,
         phone = target.searchResults[0].phone,
         address = target.searchResults[0].address,
-        roadAddress = target.searchResults[0].roadAddress,
+        road_address = target.searchResults[0].roadAddress,
         mapx = target.searchResults[0].mapx,
         mapy = target.searchResults[0].mapy,
       )
